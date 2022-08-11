34f34bcf25a5368a3c84e799fc36d3ade6cb8b18
nichibo-kiang <kiang@nichibojapan.com>
2022-08-12 11:37:11 +1200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-6hpnx6-8vg6dr
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-6hpnx6-8vg6dr)
---
+++ src/app/product-alerts/product-alerts.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Product } from '../products';
+
+@Component({
+  selector: 'app-product-alerts',
+  templateUrl: './product-alerts.component.html',
+  styleUrls: ['./product-alerts.component.css'],
+})
+export class ProductAlertsComponent {
+  constructor() {}
+
+  @Input() product!: Product;
+  @Output() notify = new EventEmitter();
+}
